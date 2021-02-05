b85f498d146afb553b77fa9160b7a19f11c87f9e
user179023 <user179023@74-80-245-37.ic2net.net>
2021-02-05 14:47:19 -0800

    profile

--- intraIsil/iPhone/Classes/Profile/ProfileViewController.swift
+++ intraIsil/iPhone/Classes/Profile/ProfileViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var SaveEditProfile: UIButton!
+    @IBOutlet var EditProfile: UIButton!
+    @IBOutlet var ImgProfile: UIImageView!
+    @IBOutlet var CloseSessionProfile: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        CloseSessionProfile.layer.cornerRadius = 10.0
+        CloseSessionProfile.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        CloseSessionProfile.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        CloseSessionProfile.layer.shadowOpacity = 1.0
+        CloseSessionProfile.layer.shadowRadius = 0.0
+        CloseSessionProfile.layer.masksToBounds = false
+        ImgProfile.layer.cornerRadius = ImgProfile.frame.size.width / 2
+        EditProfile.layer.cornerRadius = 10.0
+        EditProfile.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        EditProfile.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        EditProfile.layer.shadowOpacity = 1.0
+        EditProfile.layer.shadowRadius = 0.0
+        EditProfile.layer.masksToBounds = false
+        SaveEditProfile.layer.cornerRadius = 10.0
+        SaveEditProfile.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        SaveEditProfile.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        SaveEditProfile.layer.shadowOpacity = 1.0
+        SaveEditProfile.layer.shadowRadius = 0.0
+        SaveEditProfile.layer.masksToBounds = false
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
